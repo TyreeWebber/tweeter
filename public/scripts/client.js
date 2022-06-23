74ecc3112a3d39fad5d10d7cd53ce76fb7997d1c
Tyree Webber <webber.tyree@gmail.com>
2022-06-22 22:59:10 -0600

    fix linting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    const tweetMsg = createTweetElement(tweet);
-    $(".existing-tweets-container").append(tweetMsg);
-  }
-};
+$(document).ready(function() {
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const tweetMsg = createTweetElement(tweet);
+      $('.existing-tweets-container').prepend(tweetMsg);
+    }
+  };
+});
 
 const escape = function(str) {
   let div = document.createElement('div');
@@ -17,22 +19,31 @@ const escape = function(str) {
   return div.innerHTML;
 };
 
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const tweetMsg = createTweetElement(tweet);
+    $(".existing-tweets-container").append(tweetMsg);
+  }
+};
+
 const createTweetElement = function(tweet) {
   const timeStamp = timeago.format(tweet.created_at);
   let $tweet = `<article class="existing-tweets">
 <header class="existing-tweets-header">
-  <div class="tweeter-avatar"> <img src="${tweet.user.avatars}"/> </div>
+  
+  <img src=${tweet.user.avatars}/>
   <div class="user-name"><b>${tweet.user.name}</b></div>
   <div class="user-handle"><b>${tweet.user.handle}</b></div>
-  </header>
-<div class="existing-tweets-body"> <strong>${tweet.content.text}</strong></div>
-    <footer class="existing-tweets-footer"><b>${timeStamp}</b>
-      <div class="tweet-icons">
-      <i id="icon1" class="fas fa-flag"></i>
-      <i id="icon2" class="fas fa-retweet"></i>
-      <i id="icon3" class="fas fa-heart"></i>
-    </div>
-  </footer>
+</header>
+<div class="existing-tweets-body"> <strong>${escape(tweet.content.text)}</strong></div>
+<footer class="existing-tweets-footer"><b>${timeStamp}</b>
+  <div class="tweet-icons">
+    <i id="icon1" class="fas fa-flag"></i>
+    <i id="icon2" class="fas fa-retweet"></i>
+    <i id="icon3" class="fas fa-heart"></i>
+  </div>
+</footer>
 </article>`;
 
   return $tweet;
@@ -40,8 +51,8 @@ const createTweetElement = function(tweet) {
 
 const loadTweets = function() {
   $.ajax({ url: "/tweets", method: "GET" })
-  .then(result => renderTweets(result))
-  .catch(error => console.log(`Error:`, error));
+    .then(result => renderTweets(result))
+    .catch(error => console.log('Error:', error));
 };
 
 const submitHandler = function(event) {
@@ -50,33 +61,32 @@ const submitHandler = function(event) {
   const data = $(this).serialize();
 };
 
-  const tweetPost = function() {
-    $.ajax({ url: "/tweets", method: "POST", data: data }).then(() => {
-      $(".existing-tweets-container").empty();
-      $("#tweet-box").val("");
-      $(".alert").empty();
-      $("#counter").first().val(140);
-      loadTweets();
-    });
+const tweetPost = function() {
+  $.ajax({ url: "/tweets", method: "POST", data: data }).then(() => {
+    $(".existing-tweets-container").empty();
+    $("#tweet-box").val("");
+    $(".alert").empty();
+    $("#counter").first().val(140);
+    loadTweets();
+  });
 
-    const errorHandler = function() {
-      if (tweetBox.length === 0) {
-        $('.alert')
-          .empty()
-          .append('<p><i class="fas fa-exclamation-circle"></i><strong>Your tweet needs to be atleast 1 character long.</strong</p>');
-        $('.alert').hide().slideDown('slow');
-        error = true;
-      } else if (tweetBox.length > 140) {
-        $('.alert')
-          .empty()
-          .append('<p><i class="fas fa-exclamation-circle"></i><strong>You\'ve reached the max amount of characters!</strong></p>');
-        $('.alert').hide().slideDown('slow');
-        error = true;
-      } else {
-        tweetPost(data);
-        error = false;
-      }
-    };
-    errorHandler();
+  const errorHandler = function() {
+    if (tweetBox.length === 0) {
+      $('.alert')
+        .empty()
+        .append('<p><i class="fas fa-exclamation-circle"></i><strong>Your tweet needs to be atleast 1 character long.</strong</p>');
+      $('.alert').hide().slideDown('slow');
+      error = true;
+    } else if (tweetBox.length > 140) {
+      $('.alert')
+        .empty()
+        .append('<p><i class="fas fa-exclamation-circle"></i><strong>You\'ve reached the max amount of characters!</strong></p>');
+      $('.alert').hide().slideDown('slow');
+      error = true;
+    } else {
+      tweetPost(data);
+      error = false;
+    }
   };
-
+  errorHandler();
+};
