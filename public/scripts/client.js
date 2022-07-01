b5c776fbd1e14c2df29f95bf2443893c98ed9be6
Tyree Webber <webber.tyree@gmail.com>
2022-07-01 00:23:57 -0600

    add some comments to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,16 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
-
-  const renderTweets = function (tweets) {
+$(document).ready(function() {
+//renders tweets and prepends new tweet to tweet container
+  const renderTweets = function(tweets) {
     const $container = $(".tweets-container").empty();
     for (let tweet of tweets) {
       $container.prepend(createTweetElement(tweet));
     }
   };
 
-  const createTweetElement = function (tweet) {
+  const createTweetElement = function(tweet) {
 
     const escape = function (str) {
       const div = document.createElement('div');
@@ -21,11 +21,13 @@ $(document).ready(function () {
       return div.innerHTML;
     };
 
+//changes time from unix time to days
     const time = new Date(tweet.created_at);
     const currentTime = Date.now();
     const dayDif = Math.floor((currentTime - time) / 1000 / 60 / 60 / 24);
 
-  const tweetElement = `
+//outline for tweet container
+    const tweetElement = `
   <header class="tweet-header">
     <div class="user-info">
       <img src="${tweet.user.avatars}">
@@ -46,13 +48,14 @@ $(document).ready(function () {
   </footer>
 `;
 
+//append outline to the article class
     const $tweet = $(`<article>`);
     $tweet.addClass("article");
     const tweetArticle = $tweet.append(tweetElement);
     return tweetArticle;
   };
 
-  $(".tweet-form").submit(function (event) {
+  $(".tweet-form").submit(function(event) {
     event.preventDefault();
     const formData = $(this).serialize();
     const value = $("#tweet-text").val();
@@ -78,7 +81,7 @@ $(document).ready(function () {
     });
   });
 
-  const loadTweets = function () {
+  const loadTweets = function() {
     $.ajax({
       type: "get",
       url: "/tweets",
